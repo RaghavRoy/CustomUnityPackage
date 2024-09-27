a5db1ae73b7cc64ee7af6bc661e4def2ae41071d
RaghavRoy <raghav.mca11062@yahoo.com>
2024-09-27 17:04:06 +0530

    1.0.1

---
+++ ScenesAndScripts/JioSDK.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class JioSDK : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log("JIO SDK is working");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
