1135eaebbbc3ebc79d6e55ebc885112c990a4d3f
RaghavRoy <raghav.mca11062@yahoo.com>
2024-09-27 18:08:56 +0530

    1.0.1

---
+++ Editor/EditorExample.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QOL.Editor
+{
+    public class EditorExample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Editor Example have been called");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
