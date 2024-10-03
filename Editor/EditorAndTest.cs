2836ec3b59c647b0a6065acf8fb2e26c460c7c25
RaghavRoy <raghav.mca11062@yahoo.com>
2024-10-03 15:48:44 +0530

    u

---
+++ Editor/EditorAndTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QOL.Editor
+{
+public class EditorAndTest
+{ 
+        public interface Readable
+        {
+            void read();
+        }      
+        
+  }
+}
